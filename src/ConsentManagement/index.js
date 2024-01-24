c0200aea3060b43926458175606c45143a5b0c76
fabri <fguespe@gmail.com>
2024-01-24 12:06:57 -0300

    csv

--- src/ConsentManagement/index.js
+++ src/ConsentManagement/index.js
@@ -79,30 +79,45 @@ export function ConsentManagement({
       console.log(error);
     }
   };
+
   // Function to refresh the consent list
   const refreshConsentList = async (client) => {
+    // Fetch the consent list from the client
     let consentList = await client.contacts.refreshConsentList();
 
+    // Create a unique consent list by removing duplicates
     let uniqueConsentList = consentList
-      .slice()
-      .reverse()
+      .slice() // Create a copy of the consent list
+      .reverse() // Reverse the list to keep the latest consent
       .filter(
+        // Filter out duplicates by checking if the current index is the first occurrence of the consent value
         (consent, index, self) =>
           index === self.findIndex((t) => t.value === consent.value),
       )
-      .reverse();
+      .reverse(); // Reverse the list back to the original order
+
+    // Sort the unique consent list based on the permission type
     uniqueConsentList = uniqueConsentList.sort((a, b) => {
+      // If 'a' is allowed and 'b' is not, 'a' should come first
       if (a.permissionType === "allowed" && b.permissionType !== "allowed")
         return -1;
+      // If 'a' is unknown and 'b' is not, 'b' should come first
       if (a.permissionType === "unknown" && b.permissionType !== "unknown")
         return 1;
+      // If 'a' is denied and 'b' is not, 'b' should come first
       if (a.permissionType === "denied" && b.permissionType !== "denied")
         return 1;
+      // If none of the above conditions are met, keep the original order
       return 0;
     });
+
+    // Update the state with the unique and sorted consent list
     setConsentList(uniqueConsentList);
+
+    // Return the unique and sorted consent list
     return uniqueConsentList;
   };
+
   // Function to download the consent list as a CSV file
   const downloadCSV = async () => {
     const csvRows = [];
